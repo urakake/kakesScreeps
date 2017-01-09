fd7277f59877099381c52cea969c7e8af49dc4fc
Paul Stegemeier <Paul Stegemeier>
2017-01-09 00:22:10 -0700

    move wrok

--- role.mover.js
+++ role.mover.js
@@ -14,30 +14,34 @@ function init(creep) {
     console.log("Initializing Mover - "+creep.name);
     creep.memory.init=true;
 	if(creep.memory.sourceBin==undefined){
-	    creep.memory.sourceBin="";
+	    creep.memory.sourceBin=undefined;
 	}
 	if(creep.memory.destBin==undefined){
-	    creep.memory.destBin="";
+	    creep.memory.destBin=undefined;
 	}
-	creep.memory.targetSource="";
-    creep.memory.targetDest="";
-	creep.memory.state = "aquireEnergy";
+	creep.memory.targetSource=undefined;
+    creep.memory.targetDest=undefined;
+	creep.memory.state = "acquireEnergy";
 }
 function work(creep) {
-    if(creep.memory.state == "aquireEnergy"){
-		aquireEnergy(creep);
+    if(creep.memory.state == "acquireEnergy"){
+		acquireEnergy(creep);
 	} else if(creep.memory.state == "dumpEnergy"){
 	    dumpEnergy(creep);
 	}
 }
-function aquireEnergy(creep) {
+function acquireEnergy(creep) {
     var target;
-    if(creep.memory.sourceBin==""){
-        if(creep.memory.targetSource==""){
+    
+    if(creep.memory.sourceBin==undefined){
+        if(creep.memory.targetSource==undefined){
             
             target = findSource(creep);
         } else {
             target = Game.getObjectById(creep.memory.targetSource);
+            if((target.energy==target.energyCapacity)||(target.store==target.storeCapacity)){
+                target = findSource(creep);
+            }
         }
     } else {
         target = Game.getObjectById(creep.memory.sourceBin);
@@ -52,31 +56,33 @@ function aquireEnergy(creep) {
     }
     
     if(creep.carry.energy==creep.carryCapacity){
-        creep.memory.targetSource=""
+        creep.memory.targetSource=undefined
         creep.memory.state = "dumpEnergy";
     }
 }
 function dumpEnergy(creep) {
     var target;
-    if(creep.memory.destBin==""){
-        
-        if(creep.memory.targetDest==""){
-            
+    if(creep.memory.destBin==undefined){
+        if(creep.memory.targetDest==undefined){
             target = findDest(creep);
         } else {
+            
             target = Game.getObjectById(creep.memory.targetDest);
-            //console.log(creep.memory.targetDest)
+            //console.log(target.energy)
+            if((target.energy!=undefined)&&(target.energy==target.energyCapacity)||(target.store[RESOURCE_ENERGY]!=undefined)&&(target.store[RESOURCE_ENERGY]==target.storeCapacity)){
+                target = findDest(creep);
+            }  
+
         }
     } else {
         target = Game.getObjectById(creep.memory.destBin);
     }
-    
+    //console.log(target)
     if(target){
-        
         moveToGiveTransfer(creep,target);
     }
     if(creep.carry.energy==0){
-        creep.memory.targetDest="";
+        creep.memory.targetDest=undefined;
         creep.memory.state = "acquireEnergy";
     }
     
@@ -89,16 +95,17 @@ function voyageOutOfRoom(creep,destRoom) {
 function moveToPickup(creep,target) {
     if(target.store[RESOURCE_ENERGY]>0){
         if(creep.room.name==target.room.name){
-            
             if(creep.pickup(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target);
                     
+            } else {
+                creep.memory.targetSource=undefined;
             }
         } else {
             voyageOutOfRoom(creep,target.room)
         }
     } else {
-        creep.memory.targetSource="";
+        creep.memory.targetSource=undefined;
         //findSource(creep);
     }
 }
@@ -109,6 +116,8 @@ function moveToGetTransfer(creep,target) {
         if(creep.room.name==target.room.name){
             if(target.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target);
+            } else {
+                creep.memory.targetSource=undefined;
             }
         } else {
             voyageOutOfRoom(creep,target.room)
@@ -116,28 +125,30 @@ function moveToGetTransfer(creep,target) {
         
     } else {
         
-        creep.memory.targetSource="";
+        creep.memory.targetSource=undefined;
         //destination full
     }
 }
 function moveToGiveTransfer(creep,target) {
     //console.log(target)
-    if((target.structureType==(STRUCTURE_EXTENSION || STRUCTURE_TOWER || STRUCTURE_SPAWN) && target.energy<target.energyCapacity) ||
-        (target.structureType==(STRUCTURE_LINK || STRUCTURE_STORAGE || STRUCTURE_CONTAINER) && target.store[RESOURCE_ENERGY]<target.storeCapacity)){
+   // console.log(target.structureType==STRUCTURE_CONTAINER)
+    if((target.energy!=undefined)&&(target.energy==target.energyCapacity)||(target.store[RESOURCE_ENERGY]!=undefined)&&(target.store[RESOURCE_ENERGY]==target.storeCapacity)){
         if(creep.room.name==target.room.name){
             if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target);
+            } else {
+                creep.memory.targetDest=undefined;
             }
         } else {
             voyageOutOfRoom(creep,target.room)
         }
-        
     } else {
         findDest(creep);
     }
 }
 function findDest(creep) {
     //creep.say("find dest")
+    
     var destBins=[];
     for(var i in creep.room.memory.destBins){
         var destBin=Game.getObjectById(creep.room.memory.destBins[i]);
@@ -145,6 +156,7 @@ function findDest(creep) {
             destBins.push(destBin);
         }
     }
+    
     //    stuff that really needs energy
     var targets = creep.room.find(FIND_STRUCTURES, {
         filter: (structure) => {
@@ -154,24 +166,24 @@ function findDest(creep) {
     });
     if(targets.length){
         var target = creep.pos.findClosestByRange(targets);
-        console.log(targets.length)
         creep.memory.targetDest=target.id;
         return target;
+        
     } else {     //   find a destination bin
+    
         var target = creep.pos.findClosestByRange(destBins);
         if (target.store[RESOURCE_ENERGY]==target.storeCapacity[RESOURCE_ENERGY]){
+            
             for (var i in creep.room.memory.destBins){
                 target = Game.getObjectById(creep.room.memory.destBins[i]);
-                if (target.energy<target.energyCapacity){
+                if ((target.energy<target.energyCapacity)||(target.store<target.storeCapacity)){
                     creep.memory.targetDest=target.id;
                     return target;
                 }
             }
         } else { // closest destination bin not full
-            if(creep.memory.targetDest!=""){
-                creep.memory.targetDest=target;
-                return target;
-            }
+            creep.memory.targetDest=target.id;
+            return target;
         }
     }
 }
@@ -206,7 +218,7 @@ function findSource(creep) {
             for (var i in creep.room.memory.sourceBins){
                 target = Game.getObjectById(creep.room.memory.sourceBins[i]);
                 if (target){
-                    if (target.energy<target.energyCapacity){
+                    if  ((target.energy>0)||(target.store>0)){
                     creep.memory.targetDest=target.id;
                     return target
                     }
